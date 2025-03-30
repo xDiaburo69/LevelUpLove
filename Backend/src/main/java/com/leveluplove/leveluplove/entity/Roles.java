ee80e7f8e4d4cbe2894a152a0811d275dee158b8
Madame-parker <bonnie.boettcher@aol.com>
2025-03-30 20:55:51 +0200

    Revert "Merge branch 'main' of https://github.com/xDiaburo69/LevelUpLove"
    This reverts commit f65352788aea1f75221d6b59fc2c8026517164d8, reversing
    changes made to 94248bfc5be4bc7bf471f891a557f32f600283b0.

--- Backend/src/main/java/com/leveluplove/leveluplove/entity/Roles.java
+++
@@ -1,5 +0,0 @@
-package com.leveluplove.leveluplove.entity;
-
-// Enum (= Aufzählungstyp) für die möglichen Rollen eines Users
-// Damit kann man später zwischen normalen Nutzern und Admins unterscheiden
-public enum Roles { USER, ADMIN }
