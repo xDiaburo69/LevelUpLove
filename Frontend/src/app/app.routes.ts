c6f585363caf0e82382d454bc1fe2f215da99911
Madame-parker <bonnie.boettcher@aol.com>
2025-04-17 14:08:18 -0700

    fix routing add about us site add log and reg add contact sit

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component'; // Importiere die neue Komponente
 
 export const routes: Routes = [
-    {path:"", component:HeaderComponent},
-    {path:"about-us", component:AboutUsComponent},
+  { path: '', component: HeaderComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  {path: 'contact', component: ContactComponent}, // Füge den neuen Pfad hier hinzu
 ];
---
+++ Frontend/src/app/login-reg/login-reg.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginRegComponent } from './login-reg.component';
+
+describe('LoginRegComponent', () => {
+  let component: LoginRegComponent;
+  let fixture: ComponentFixture<LoginRegComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LoginRegComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginRegComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
